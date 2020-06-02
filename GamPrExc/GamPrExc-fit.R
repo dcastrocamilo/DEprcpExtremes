1f752f70cf6d8166bd15f4ed4173df0f1139c104
Daniela Castro Camilo <danielacastrocamilo@Danielas-MacBook-Pro.local>
2020-06-02 16:04:51 +0100

    Include output from gam.check().

--- GamPrExc/GamPrExc-fit.R
+++ GamPrExc/GamPrExc-fit.R
@@ -26,3 +26,22 @@ b = gam(obs ~ s(elev) + te(lon,lat) + s(month, bs="cc", k=12) + s(year),
 Sys.time() - t0 # 
 gam.check(b) 
 save(b, file = 'GamPrExcSpatioTemporal112stations_elev9_te24_year9.Rdata')
+
+# Gam check:
+# Method: UBRE   Optimizer: outer newton
+# full convergence after 7 iterations.
+# Gradient range [-1.757037e-10,1.469602e-07]
+# (score 0.2230398 & scale 1).
+# Hessian positive definite, eigenvalue range [4.728343e-10,4.171086e-07].
+# Model rank =  53 / 53
+# 
+# Basis dimension (k) checking results. Low p-value (k-index<1) may
+# indicate that k is too low, especially if edf is close to k'.
+# 
+#                k'   edf k-index p-value
+# s(elev)      9.00  8.46    1.00    0.93
+# te(lon,lat) 24.00 23.55    0.99    0.53
+# s(month)    10.00  9.93    0.98    0.20
+# s(year)      9.00  8.97    0.97    0.14
+
+  
