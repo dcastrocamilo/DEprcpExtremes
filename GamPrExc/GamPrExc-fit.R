e1198ca1c963f68c5066ed8ecd3672c9fbd20254
Daniela Castro Camilo <danielacastrocamilo@Danielas-MacBook-Pro.local>
2020-06-03 22:16:58 +0100

    Fit GAMs to probability of precipitation.

--- GamPrExc/GamPrExc-fit.R
+++ GamPrExc/GamPrExc-fit.R
@@ -5,27 +5,28 @@ library(mgcv)
 load('german-precipitation.rdata')
 load('sites_DE_sel_elev.Rdata') # 4527 sites (columns), 14030 time points (rows)
 load('SpatialSample.Rdata')
-load('GamData_ber.Rdata')
+load('GamPrExc/fitted_quant_75.rdata')
 
-dat = data.frame("obs"=as.vector(data_ber),
-                 "site"=rep(1:ncol(data_ber), each= nrow(data_ber)),
-                 "lat"=rep(sites_DE_sel$lat, each= nrow(data_ber)),
-                 "lon"=rep(sites_DE_sel$lon, each= nrow(data_ber)),
-                 "elev"=rep(sites_DE_sel$elevation, each= nrow(data_ber)),
-                 "month"=rep(dates_sel$month, ncol(data_ber)),
-                 "year"=rep(dates_sel$year, ncol(data_ber)))
+dat = data.frame("obs"=as.vector(fitted.quant.mat),
+                 "site"=rep(1:ncol(fitted.quant.mat), each= nrow(fitted.quant.mat)),
+                 "lat"=rep(sites_DE_sel$lat, each= nrow(fitted.quant.mat)),
+                 "lon"=rep(sites_DE_sel$lon, each= nrow(fitted.quant.mat)),
+                 "elev"=rep(sites_DE_sel$elevation, each= nrow(fitted.quant.mat)),
+                 "month"=rep(dates_sel$month, ncol(fitted.quant.mat)),
+                 "year"=rep(dates_sel$year, ncol(fitted.quant.mat)))
 
 site2keep = spatial.sample$id
 dat = dat[which(dat$site %in% site2keep), ]
+save(dat, file = 'GamPrExc/GamData_isExcQ75.Rdata')
 
 t0 = Sys.time()
 b = gam(obs ~ s(elev) + te(lon,lat) + s(month, bs="cc", k=12) + s(year), 
         family = binomial, 
         data = dat, 
         select = TRUE)
-Sys.time() - t0 # 
+Sys.time() - t0 # 18 mins
 gam.check(b) 
-save(b, file = 'GamPrExcSpatioTemporal112stations_elev9_te24_year9.Rdata')
+save(b, file = 'GamPrExcQ75SpatioTemporal112stations_elev9_te24_year9.Rdata')
 
 # Gam check:
 # Method: UBRE   Optimizer: outer newton
