e92de6b27c630a851cddebec10a1ac40fd8a64cd
Daniela Castro Camilo <danielacastrocamilo@Danielas-MacBook-Pro.local>
2020-06-04 12:36:27 +0100

    Updated to include new matrix sent by Linda.

--- GamPrExc/GamPrExc-fit.R
+++ GamPrExc/GamPrExc-fit.R
@@ -5,19 +5,22 @@ library(mgcv)
 load('german-precipitation.rdata')
 load('sites_DE_sel_elev.Rdata') # 4527 sites (columns), 14030 time points (rows)
 load('SpatialSample.Rdata')
-load('GamPrExc/fitted_quant_75.rdata')
+# load('GamPrExc/GamData_isExcQ75.Rdata')
+load('GamData_isExcQ75.Rdata')
 
-dat = data.frame("obs"=as.vector(fitted.quant.mat),
-                 "site"=rep(1:ncol(fitted.quant.mat), each= nrow(fitted.quant.mat)),
-                 "lat"=rep(sites_DE_sel$lat, each= nrow(fitted.quant.mat)),
-                 "lon"=rep(sites_DE_sel$lon, each= nrow(fitted.quant.mat)),
-                 "elev"=rep(sites_DE_sel$elevation, each= nrow(fitted.quant.mat)),
-                 "month"=rep(dates_sel$month, ncol(fitted.quant.mat)),
-                 "year"=rep(dates_sel$year, ncol(fitted.quant.mat)))
-
-site2keep = spatial.sample$id
-dat = dat[which(dat$site %in% site2keep), ]
-save(dat, file = 'GamPrExc/GamData_isExcQ75.Rdata')
+# load('GamPrExc/GamData_isExcQ75_14030x4527.Rdata')
+# 
+# dat = data.frame("obs"=as.vector(data_ber),
+#                  "site"=rep(1:ncol(data_ber), each= nrow(data_ber)),
+#                  "lat"=rep(sites_DE_sel$lat, each= nrow(data_ber)),
+#                  "lon"=rep(sites_DE_sel$lon, each= nrow(data_ber)),
+#                  "elev"=rep(sites_DE_sel$elevation, each= nrow(data_ber)),
+#                  "month"=rep(dates_sel$month, ncol(data_ber)),
+#                  "year"=rep(dates_sel$year, ncol(data_ber)))
+# 
+# site2keep = spatial.sample$id
+# dat = dat[which(dat$site %in% site2keep), ]
+# save(dat, file = 'GamPrExc/GamData_isExcQ75.Rdata')
 
 t0 = Sys.time()
 b = gam(obs ~ s(elev) + te(lon,lat) + s(month, bs="cc", k=12) + s(year), 
