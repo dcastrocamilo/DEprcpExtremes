9e8c39ce07c00f57f313c68c3dee06f127eb8994
Daniela Castro Camilo <danielacastrocamilo@Danielas-MacBook-Pro.local>
2020-06-04 16:35:24 +0100

    Goodness of fit for exceedance probabilities.

--- GamPrExc/GamPrExc-GoF.R
+++ GamPrExc/GamPrExc-GoF.R
@@ -7,25 +7,26 @@ library("viridis")
 library(schoenberg)
 load("german-precipitation.RData")
 load("sites_DE_sel_elev.Rdata")
-load('GamPrExc/GamData_ber.Rdata')
+load('GamPrExc/GamData_isExcQ75.Rdata')
+st = 22
 
-dat = data.frame("obs"=as.vector(data_ber),
-                 "site"=rep(1:ncol(data_ber), each= nrow(data_ber)),
-                 "lat"=rep(sites_DE_sel$lat, each= nrow(data_ber)),
-                 "lon"=rep(sites_DE_sel$lon, each= nrow(data_ber)),
-                 "elev"=rep(sites_DE_sel$elevation, each= nrow(data_ber)),
-                 "month"=rep(dates_sel$month, ncol(data_ber)),
-                 "year"=rep(dates_sel$year, ncol(data_ber)))
 
-name = 'GamPrExc/GamPrExcSpatioTemporal112stations_elev9_te24_year9.Rdata'
-load(name) 
+name = 'GamPrExc/GamPrExcSpatioTemporal112stations_elev9_te24_year9'
+load(paste0(name,'.Rdata') )
 out = b
 fit = out$fitted.values
 
 ## Chi-square goodness of fit test
+# H0: the model is correct
 r = (out$y - fit)/(sqrt(out$fitted.values*(1-out$fitted.values)))
 n = length(out$y); p = 4
-1- pchisq(sum(r^2), df = (n-p+1)) # we cannot reject the null hypothesis that this model is correct
+chi2 = 1- pchisq(sum(r^2), df = (n-p+1)); chi2 # > 0.05 we cannot reject the null hypothesis that this model is correct
+
+## Hosmer-Lemeshow Test
+# H0: the model fits the data
+library(ResourceSelection)
+hl = hoslem.test(out$y, fitted(out), g = 10); hl 
+
 
 # Strategy to calculate the Hosmer-Lemeshow groupings:
 # Form a matrix with the outcome and fitted values,
@@ -35,7 +36,7 @@ hosmer = matrix(c(out$y[index], fit[index]), byrow=F, ncol=2)
 
 # Now to group into 10 groups each with m=20 observations, say, and graph:
 observed = predicted = rep(NA, 10)
-m = floor(1551309/10)
+m = floor(nrow(hosmer)/10)
 for (i in 1:10) {
   observed[i] <- sum(hosmer[(m*(i-1)+1):(m*i),1])/m
   predicted[i] <- sum(hosmer[(m*(i-1)+1):(m*i),2])/m
@@ -43,11 +44,10 @@ for (i in 1:10) {
 # the first group consists of the observations with the lowest 10% predicted probabilities. 
 # The second group consists of the 10% of the sample whose predicted probabilities are next smallest, etc etc.
 
+pdf(paste0(name, '-GoF.pdf'))
 plot(predicted, observed, type="b")
 abline(0, 1, col = 2)
-
-## Hosmer-Lemeshow Test
-# H0: the model fits the data
-library(ResourceSelection)
-hoslem.test(out$y, fitted(out), g = 10) # reject H0. Might be because of sample size
+mtext(paste('Chi-squared:', round(chi2,4)), side = 1, line = -6)
+mtext(paste('HL:', round(hl$p.value,6)), side = 1, line = -5)
+dev.off()
 
